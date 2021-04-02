dd52542fa52bb468427e424b8f7f6329e20c4d24
Anna Opareva <anna.opareva@akvelon.com>
2021-04-02 14:21:57 +0300

    add node

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-To start project you just need to clone repo and open index.html
+To start project you just need:
+- clone repo
+- set environment variables: API_URL, API_TOKEN, URL_LOG 
+- Use **npm i** for install dependencies
+- Use **npm start** for run 
+
+
+сейчас переменные окружения доступны сразу. Но правильно указать их в gitignore.
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// если делаем с node.js, то выносим переменные в .env и .env прописываем в .gitignore
 
-const API_URL = "https://api.appcenter.ms/v0.1/apps/anna.opareva/wewe/branches";
-const API_TOKEN = "5fd058c40fa1fd01fb7271ee4fde2c0e032b9a1d";
-const URL_LOG =
-  "https://appcenter.ms/users/anna.opareva/apps/wewe/build/branches/";
-
-const ul = document.createElement("ul");
-document.body.append(ul);
+const fetch = require("node-fetch");
+require('dotenv').config()
 
 async function getData() {
   const branchData = [];
   try {
-    const res = await fetch(API_URL, {
+    const res = await fetch(process.env.API_URL, {
       headers: {
-        "X-API-Token": API_TOKEN,
+        "X-API-Token": process.env.API_TOKEN,
       },
     });
     const JsonFromApi = await res.json();
@@ -39,15 +33,11 @@ async function getData() {
 function outPutData(data) {
   data.forEach((element) => {
     const { name, status, timeBuild, id } = element;
-    const linkLog = `${URL_LOG}${name}/builds/${id}`;
+    const linkLog = `${process.env.URL_LOG}${name}/builds/${id}`;
     const resultForConsole = `Branch ${name} build ${status} in ${timeBuild} seconds. Link to build logs: ${linkLog}`;
     const resultForWeb = `Branch ${name} build ${status} in ${timeBuild} seconds. Link to build logs: <a href =${linkLog}> LOGI </a>`;
 
     console.log(resultForConsole);
-
-    const li = document.createElement("li");
-    li.innerHTML = `<li>${resultForWeb}</li>`;
-    ul.append(li);
   });
 }
 
